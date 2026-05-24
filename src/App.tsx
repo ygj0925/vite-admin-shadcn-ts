ca9a6f7cce6af98e55586b8c6576756b41b2487e
yuanguangji <1351527793@163.com>
2026-05-24 23:15:30 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>点击我</Button>
+    </div>
+  )
+}
+
+export default App
